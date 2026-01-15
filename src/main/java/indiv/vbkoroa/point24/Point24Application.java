1966e3a20cb07f96e4bdcd552ee2b2fda9020ad7
abko <abko121115@gmail.com>
2026-01-15 12:02:16 +0000

    init: 프로젝트 초기화

---
+++ src/main/java/indiv/vbkoroa/point24/Point24Application.java
@@ -0,0 +1,13 @@
+package indiv.vbkoroa.point24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Point24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Point24Application.class, args);
+	}
+
+}
---
+++ src/test/java/indiv/vbkoroa/point24/Point24ApplicationTests.java
@@ -0,0 +1,13 @@
+package indiv.vbkoroa.point24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Point24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
